8c9db8c2b770797efc4361ebbca41ec0d7cc56ac
Bui The Hoa <buithehoa@gmail.com>
2025-08-04 12:24:37 +0700

    Name the problem.

--- Arrays/src/ContainerWithMostWater.java
+++ Arrays/src/ContainerWithMostWater.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Solution {
+public class ContainerWithMostWater {
     public int maxArea(int[] height) {
         return 0;
     }
