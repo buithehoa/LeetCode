f46a27ebbf63ac437b799fc14c3d7b94173fe58c
Bui The Hoa <buithehoa@gmail.com>
2025-08-04 12:23:10 +0700

    Add a blank class.

---
+++ Arrays/src/Solution.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Solution {
+    public int maxArea(int[] height) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
