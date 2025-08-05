f3592bf9594f86bfa6dd8b28e9e8c0d867c10953
Bui The Hoa <buithehoa@gmail.com>
2025-08-05 22:29:35 +0700

    Sort the input.

--- Arrays/src/com/leetcode/arrays/ThreeSum.java
+++ Arrays/src/com/leetcode/arrays/ThreeSum.java
@@ -1,10 +1,12 @@
 package com.leetcode.arrays;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
 
         return new ArrayList<List<Integer>>();
     }
