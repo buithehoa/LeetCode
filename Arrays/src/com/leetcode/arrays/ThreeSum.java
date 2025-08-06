f57043ff0234c2003889ed2a752ddf4dff890346
Bui The Hoa <buithehoa@gmail.com>
2025-08-06 14:34:17 +0700

    Optimize some memory usage.

--- Arrays/src/com/leetcode/arrays/ThreeSum.java
+++ Arrays/src/com/leetcode/arrays/ThreeSum.java
@@ -12,12 +12,15 @@ public class ThreeSum {
         List<List<Integer>> triplets = new ArrayList<>();
         Arrays.sort(nums);
 
+        boolean continued;
         int left, right, sum;
         List<Integer> triplet;
+        int lastIndex = nums.length - 1;
+
         for (int i = 0; i < nums.length - 2; i++) {
-            boolean continued = true;
+            continued = true;
             left = i + 1;
-            right = nums.length - 1;
+            right = lastIndex;
 
             while (continued) {
                 sum = nums[i] + nums[left] + nums[right];
