0c0ba5cef2bfde8fc3934cebe0dfa9aecd3bc1b2
Bui The Hoa <buithehoa@gmail.com>
2025-08-06 14:03:03 +0700

    Implement the solution.

--- Arrays/src/com/leetcode/arrays/ThreeSum.java
+++ Arrays/src/com/leetcode/arrays/ThreeSum.java
@@ -1,3 +1,6 @@
+/*
+ * https://leetcode.com/problems/3sum?envType=problem-list-v2&envId=n4p5um33
+ */
 package com.leetcode.arrays;
 
 import java.util.ArrayList;
@@ -6,8 +9,31 @@ import java.util.List;
 
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> triplets = new ArrayList<>();
         Arrays.sort(nums);
 
-        return new ArrayList<List<Integer>>();
+        int left, right, sum;
+        for (int i = 0; i < nums.length - 2; i++) {
+            boolean continued = true;
+            left = i + 1;
+            right = nums.length - 1;
+
+            while (continued) {
+                sum = nums[i] + nums[left] + nums[right];
+
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    triplets.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                }
+
+                if (left == right) continued = false;
+            }
+        }
+
+        return triplets;
     }
 }
