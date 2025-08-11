551dd3b84fe17041840afca7994bdd4b1010d1bc
Bui The Hoa <buithehoa@gmail.com>
2025-08-11 16:47:16 +0700

    Add a daft solution and tests.

--- Arrays/src/com/leetcode/arrays/NextPermutation.java
+++ Arrays/src/com/leetcode/arrays/NextPermutation.java
@@ -1,7 +1,22 @@
 package com.leetcode.arrays;
 
+import java.util.Arrays;
+
 public class NextPermutation {
     public void nextPermutation(int[] nums) {
 
+        outerLoop:
+        for (int i = nums.length - 1; i > 0; i--) {
+            for (int j = i - 1; j > 0; j--) {
+                if (nums[i] > nums[j]) {
+                    int tmp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = tmp;
+
+                    Arrays.sort(nums, j, nums.length - 1);
+                    break outerLoop;
+                }
+            }
+        }
     }
 }
