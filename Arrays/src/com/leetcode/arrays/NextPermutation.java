ff294570f25dfa23ebc4a23488585e9a75d000d1
Bui The Hoa <buithehoa@gmail.com>
2025-08-14 21:08:58 +0700

    Remove old implementation.

--- Arrays/src/com/leetcode/arrays/NextPermutation.java
+++ Arrays/src/com/leetcode/arrays/NextPermutation.java
@@ -27,25 +27,4 @@ public class NextPermutation {
             }
         }
     }
-
-    public void nextPermutationX(int[] nums) {
-        outerLoop:
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (i == 0) {
-                Arrays.sort(nums);
-                break;
-            }
-
-            for (int j = i - 1; j >= 0; j--) {
-                if (nums[i] > nums[j]) {
-                    int tmp = nums[j];
-                    nums[j] = nums[i];
-                    nums[i] = tmp;
-
-                    Arrays.sort(nums, j + 1, nums.length);
-                    break outerLoop;
-                }
-            }
-        }
-    }
 }
