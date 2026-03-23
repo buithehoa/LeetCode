92d2ba018c8e4052abc0e8aad6eb272c98c76ac9
Bui The Hoa <buithehoa@gmail.com>
2026-03-23 18:12:50 +0700

    Add a improved version.

--- merge-strings-alternately/solution.rb
+++ merge-strings-alternately/solution.rb
@@ -2,22 +2,14 @@
 # @param {String} word2
 # @return {String}
 def merge_alternately(word1, word2)
-  length = word1.length <= word2.length ? word1.length : word2.length
-  short_word = word1
-  long_word = word2
-
-  if word1.length > word2.length
-    short_word = word2
-    long_word = word1
-  end
-
-  merged = ""
-
-  for i in 0...short_word.length
-    merged = "#{merged}#{word1[i]}#{word2[i]}"
-  end
-
-  merged = "#{merged}#{long_word[short_word.length..long_word.length]}"
-
-  merged
+   i = 0
+   merged = +""
+ 
+   while i < word1.length || i < word2.length
+     merged << word1[i] if i < word1.length
+     merged << word2[i] if i < word2.length
+     i += 1
+   end
+ 
+   merged
 end
