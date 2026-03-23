15552c906edaffe62938ad161eb696b6c599c4ce
Bui The Hoa <buithehoa@gmail.com>
2026-03-23 18:27:33 +0700

    Format the code.

--- merge-strings-alternately/solution.rb
+++ merge-strings-alternately/solution.rb
@@ -2,14 +2,14 @@
 # @param {String} word2
 # @return {String}
 def merge_alternately(word1, word2)
-   i = 0
-   merged = +""
- 
-   while i < word1.length || i < word2.length
-     merged << word1[i] if i < word1.length
-     merged << word2[i] if i < word2.length
-     i += 1
-   end
- 
-   merged
+  i = 0
+  merged = +""
+
+  while i < word1.length || i < word2.length
+    merged << word1[i] if i < word1.length
+    merged << word2[i] if i < word2.length
+    i += 1
+  end
+
+  merged
 end
