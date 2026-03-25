de05d35772dc7c6c7a1f4b49601619b69d10d016
Bui The Hoa <buithehoa@gmail.com>
2026-03-25 15:46:23 +0700

    Add an initial solution.

---
+++ greatest-common-divisor-of-strings/solution.rb
@@ -0,0 +1,21 @@
+# @param {String} str1
+# @param {String} str2
+# @return {String}
+def gcd_of_strings(str1, str2)
+    str = str1.length < str2.length ? str1 : str2;
+
+    divisors = []
+    divisor = ""
+    str.each_char do |c|
+      divisor += c
+
+      is_divisor1 = str1 == divisor * (str1.length / divisor.length)
+      is_divisor2 = str2 == divisor * (str2.length / divisor.length)
+
+      if is_divisor1 && is_divisor2
+        divisors << divisor
+      end
+    end
+
+    divisors.length == 0 ? "" : divisors.last
+end
