d81b24eab0431d98f373868f3e2a312cc8ea1900
Bui The Hoa <buithehoa@gmail.com>
2026-03-30 21:42:23 +0700

    Add the improved version.

--- greatest-common-divisor-of-strings/solution.rb
+++ greatest-common-divisor-of-strings/solution.rb
@@ -2,22 +2,12 @@
 # @param {String} str2
 # @return {String}
 def gcd_of_strings(str1, str2)
-    str = str1.length < str2.length ? str1 : str2;
-
-    divisors = []
-    divisor = ""
-    str.each_char do |c|
-      divisor += c
-
-      next if (str1.length % divisor.length != 0 || str2.length % divisor.length != 0)
-
-      is_divisor1 = str1 == divisor * (str1.length / divisor.length)
-      is_divisor2 = str2 == divisor * (str2.length / divisor.length)
-
-      if is_divisor1 && is_divisor2
-        divisors << divisor
-      end
-    end
-
-    divisors.length == 0 ? "" : divisors.last
+    # Early exit: if concatenations don't match, no common divisor exists
+    return "" if str1 + str2 != str2 + str1
+    
+    # The GCD string length must be the GCD of both string lengths
+    gcd_length = str1.length.gcd(str2.length)
+    
+    # Return the prefix of that length
+    str1[0...gcd_length]
 end
